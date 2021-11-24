69f1db528f81d0b77c4edbc0b59ca3b8453d69bb
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-11-24 17:40:03 +0300

    ~~ Remove incorrect optimization ~~

--- compiler/fir/java/src/org/jetbrains/kotlin/fir/java/enhancement/SignatureEnhancement.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/enhancement/SignatureEnhancement.kt
@@ -384,6 +384,9 @@ class FirSignatureEnhancement(
         ownerParameter: FirJavaValueParameter,
         index: Int
     ): FirResolvedTypeRef {
+        if (ownerParameter.returnTypeRef is FirResolvedTypeRef) {
+            return ownerParameter.returnTypeRef as FirResolvedTypeRef
+        }
         return ownerFunction.enhanceValueParameter(
             overriddenMembers,
             ownerParameter,
